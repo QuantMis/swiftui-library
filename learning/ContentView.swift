b09bfccafa44a64418d2971eea8fc2dff984630f
quantmis <realhost01@gmail.com>
2023-09-05 11:04:31 +0800

    Initial Commit

---
+++ learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning
+//
+//  Created by Israa on 05/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Israa on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
